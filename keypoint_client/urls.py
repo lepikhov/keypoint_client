6236d120705a2bbad578b8971450bc85437a424f
Pavel Lepikhov <lepikhovpv@gmail.com>
2023-01-08 19:08:41 +0300

    bug with MEDIA for DEBUG=False is fixed, 404 template is added

--- keypoint_client/urls.py
+++ keypoint_client/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import serve, static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +25,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns.append(re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}))    
